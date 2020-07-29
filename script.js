0f5a74044f402ad5e45f838de4f945951ffead2f
Bnjackson <65509144+Bnjackson@users.noreply.github.com>
2020-07-29 15:36:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -147,10 +147,30 @@ let func = function(arg1, arg2, ...argN) {
   return expression;
 };
 
-//OBJECTS - Are used to group related data and functions together.
+/*OBJECTS - Objects in Comparison to primitive data types are used to store collections of various data types and more complex entities.
+Objects are created using {} a propety is a 'key value' pair and the value can be anything. key: value.
+*/
+const ben = { // an object
+  firstName:'Ben', //by key 'firstName' value 'ben'
+  lastName:'Jackson', //by key 'lastName' value 'jackson'
+  age:24}; //by key 'age' store value 30
 
-let ben = {firstName:'Ben',
-lastName:'Jackson',
- age:'24'}
 
 console.log(ben);
+
+/*this keyword - Refers to the object it belongs to.
+This has different values depending on where it is used
+  In a method this refers to the object owner.
+  Alone this refers to the global object.
+  In a function, this refers to the global object.
+  In a function, in strict mode, this is undefined.
+  In an event, this refers to the element that received the event.
+  Methods like call(), and apply() can refer this to any object.
+*/
+//Methods -
+const car = {
+  carName: 'kia',
+  sayName: function() {
+    console.log(this.carName);
+  }
+}
